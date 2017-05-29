ee6451c03271157a8925ffdf75365b7968836def
Nguyen Tuan Vinh <Nguyen Tuan Vinh>
2017-05-29 22:54:00 +0700

    test commit

---
+++ testprog/src/main/java/com/vntprograming/spring/test/testprog/App.java
@@ -0,0 +1,13 @@
+package com.vntprograming.spring.test.testprog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
